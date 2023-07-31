8c1e7246067ac1519c783e32767e45f61844efaa
Mehedi Hasan <mehedinahid27@gmail.com>
2023-07-31 21:37:50 +0600

    added json renderer

--- core/settings.py
+++ core/settings.py
@@ -154,10 +154,11 @@ CKEDITOR_CONFIGS = {
 
 
 
-# REST_FRAMEWORK = {
-#     'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
-#     'PAGE_SIZE': 10,  # Number of items displayed per page
-# }
+REST_FRAMEWORK = {
+    'DEFAULT_RENDERER_CLASSES': [
+        'rest_framework.renderers.JSONRenderer',
+    ]
+}
 
 
 CORS_ALLOWED_ORIGINS = [
